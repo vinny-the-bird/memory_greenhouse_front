6ea49ecb90c83d1fe244dc586ef5a21ea8cc027d
Vincent-Jung <117348000+Vincent-Jung@users.noreply.github.com>
2025-07-17 12:27:14 +0200

    init main

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <header></header>
+  <main>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <h1>Memory - Project Greenhouse</h1>
+      <h2>test dist</h2>
+      <img src="\src\assets\yang_silicon_valley.jpg" alt="" />
+    </div>
+  </main>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
