ae57e30515e1c8823d24567ebda0ebb2568f36ec
Vincent-Jung <117348000+Vincent-Jung@users.noreply.github.com>
2025-09-08 16:02:43 +0200

    init branch develop

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
       <h1>Memory - Project Greenhouse</h1>
-      <h2>test dist</h2>
+      <h2>hello world</h2>
       <img src="\src\assets\yang_silicon_valley.jpg" alt="" />
+
     </div>
   </main>
 </template>
