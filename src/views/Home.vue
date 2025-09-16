d725686155927fd8557d1666c59be3438ca9850f
Vincent-Jung <117348000+Vincent-Jung@users.noreply.github.com>
2025-09-16 16:56:49 +0200

    added font awesome library. added a few icons: logout, info, add new. WIP home page has a temporary size preview with cup of coffee icon.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,31 @@
 <template>
   <Navbar />
-  <h1>Page of Home</h1>
+  <div class="container px-2">
+    <h2 class="title is-4">Green House Homepage</h2>
+
+    <div>
+      <p>As the day goes ..........</p>
+      <!-- X-factor sizing -->
+      <font-awesome-icon icon="fa-solid fa-coffee" />
+      <font-awesome-icon icon="fa-solid fa-coffee" size="2x" />
+      <font-awesome-icon icon="fa-solid fa-coffee" size="3x" />
+      <font-awesome-icon icon="fa-solid fa-coffee" size="4x" />
+      <font-awesome-icon icon="fa-solid fa-coffee" size="5x" />
+      <font-awesome-icon icon="fa-solid fa-coffee" size="6x" />
+      <font-awesome-icon icon="fa-solid fa-coffee" size="7x" />
+      <font-awesome-icon icon="fa-solid fa-coffee" size="8x" />
+      <font-awesome-icon icon="fa-solid fa-coffee" size="9x" />
+      <font-awesome-icon icon="fa-solid fa-coffee" size="10x" />
+    </div>
+  </div>
 </template>
 
 <script setup>
 import Navbar from "@/components/Navbar.vue";
 </script>
 
-<style scoped></style>
+<style scoped>
+.px-2 {
+  margin-bottom: 3rem;
+}
+</style>
